4faf9501ff2a100cd62d5bba8ebf86fcfa507cf4
Paulo Diovani Gonçalves <paulo@diovani.com>
2015-11-25 12:00:54 -0200

    Initialize app on test bootstrap

--- test/bootstrap.test.js
+++ test/bootstrap.test.js
@@ -1,2 +1,10 @@
+process.env.NODE_ENV = 'test';
+
 global.expect  = require('chai').expect;
 global.request = require('supertest');
+global.app     = require('../app');
+
+var http   = require('http');
+var server = http.createServer(global.app);
+
+server.listen(3100);
